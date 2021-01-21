1c8ccc5910c3ba49c89fc563d76d2a0f9c928c7e
simran <bhalla.simran297@gmail.com>
2021-01-21 21:50:13 +0530

    done

--- src/components/CreationCard.js
+++ src/components/CreationCard.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 export default function CreationCard(props) {
+  console.log(props.details.imgUrl);
   return (
     <div
       className="card_container"
       style={{
-        backgroundImage: `url("${require("../images/" + props.details.imgUrl + "')})`,
+        background: `url("${
+          process.env.PUBLIC_URL + "/images/" + props.details.imgUrl
+        }")`,
         backgroundRepeat: "no-repeat",
         width: "250px",
-        height: "250px",
+        height: "300px",
+        color: "#fff",
       }}
     >
-      <h1>{props.details.heading}</h1>
+      <div className="text_bottom">
+        <h1
+          style={{
+            marginTop: "80%",
+            padding: "10px",
+
+            bold: "800",
+          }}
+        >
+          {props.details.heading}
+        </h1>
+      </div>
     </div>
   );
 }
